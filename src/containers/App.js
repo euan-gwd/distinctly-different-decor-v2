655e170de9a1ba37023f362ebb55c2446f6665e5
euan-gwd <euan@amwic.co.za>
2017-12-06 16:03:19 +0200

    convert App back to Stateful component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import styled from 'styled-components';
 
 import ProductsList from '../components/ProductsList';
 import ProductDetail from '../components/ProductDetail';
 
-const App = () => {
-  return (
-    <AppContainer>
-      <Switch>
-        <Route exact path="/" component={ProductsList} />
-        <Route path="/:id" component={ProductDetail} />
-      </Switch>
-    </AppContainer>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <AppContainer>
+        <Switch>
+          <Route exact path="/" component={ProductsList} />
+          <Route path="/:id" component={ProductDetail} />
+        </Switch>
+      </AppContainer>
+    );
+  }
+}
 
 export default App;
 
