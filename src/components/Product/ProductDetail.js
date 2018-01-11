1727843cd8fbc6e18dcd13eb41a92bf915e563ae
euan-gwd <euan@amwic.co.za>
2018-01-11 13:47:33 +0200

    reorganise component

--- src/components/Product/ProductDetail.js
+++ src/components/Product/ProductDetail.js
@@ -76,62 +76,6 @@ class ProductDetail extends Component {
       showSuccessMessage
     } = this.state;
 
-    const sizeOptions = [
-      { key: 'sm', text: 'Small', value: 'S' },
-      { key: 'md', text: 'Medium', value: 'M' },
-      { key: 'lg', text: 'Large', value: 'L' }
-    ];
-
-    const colorOptions = [
-      { key: 1, text: 'Red', value: 'Red', label: { color: 'red', empty: true, circular: true } },
-      {
-        key: 2,
-        text: 'Green',
-        value: 'Green',
-        label: { color: 'green', empty: true, circular: true }
-      },
-      {
-        key: 3,
-        text: 'Blue',
-        value: 'Blue',
-        label: { color: 'blue', empty: true, circular: true }
-      },
-      {
-        key: 4,
-        text: 'Yellow',
-        value: 'Yellow',
-        label: { color: 'yellow', empty: true, circular: true }
-      },
-      {
-        key: 5,
-        text: 'Black',
-        value: 'Black',
-        label: { color: 'black', empty: true, circular: true }
-      }
-    ];
-
-    const qtyOptions = [
-      { key: 1, text: '1', value: '1' },
-      { key: 2, text: '2', value: '2' },
-      { key: 3, text: '3', value: '3' },
-      { key: 4, text: '4', value: '4' },
-      { key: 5, text: '5', value: '5' },
-      { key: 6, text: '6', value: '6' },
-      { key: 7, text: '7', value: '7' },
-      { key: 8, text: '8', value: '8' },
-      { key: 9, text: '9', value: '9' },
-      { key: 10, text: '10', value: '10' },
-      { key: 20, text: '20', value: '20' },
-      { key: 30, text: '30', value: '30' },
-      { key: 40, text: '40', value: '40' },
-      { key: 50, text: '50', value: '50' },
-      { key: 60, text: '60', value: '60' },
-      { key: 70, text: '70', value: '70' },
-      { key: 80, text: '80', value: '80' },
-      { key: 90, text: '90', value: '90' },
-      { key: 100, text: '100', value: '100' }
-    ];
-
     const Pricing = formatPrice(product.price) + ' each';
 
     return (
@@ -319,3 +263,59 @@ export const ButtonGroup = styled.div`
     grid-column-gap: 1rem;
   }
 `;
+
+const sizeOptions = [
+  { key: 'sm', text: 'Small', value: 'S' },
+  { key: 'md', text: 'Medium', value: 'M' },
+  { key: 'lg', text: 'Large', value: 'L' }
+];
+
+const colorOptions = [
+  { key: 1, text: 'Red', value: 'Red', label: { color: 'red', empty: true, circular: true } },
+  {
+    key: 2,
+    text: 'Green',
+    value: 'Green',
+    label: { color: 'green', empty: true, circular: true }
+  },
+  {
+    key: 3,
+    text: 'Blue',
+    value: 'Blue',
+    label: { color: 'blue', empty: true, circular: true }
+  },
+  {
+    key: 4,
+    text: 'Yellow',
+    value: 'Yellow',
+    label: { color: 'yellow', empty: true, circular: true }
+  },
+  {
+    key: 5,
+    text: 'Black',
+    value: 'Black',
+    label: { color: 'black', empty: true, circular: true }
+  }
+];
+
+const qtyOptions = [
+  { key: 1, text: '1', value: '1' },
+  { key: 2, text: '2', value: '2' },
+  { key: 3, text: '3', value: '3' },
+  { key: 4, text: '4', value: '4' },
+  { key: 5, text: '5', value: '5' },
+  { key: 6, text: '6', value: '6' },
+  { key: 7, text: '7', value: '7' },
+  { key: 8, text: '8', value: '8' },
+  { key: 9, text: '9', value: '9' },
+  { key: 10, text: '10', value: '10' },
+  { key: 20, text: '20', value: '20' },
+  { key: 30, text: '30', value: '30' },
+  { key: 40, text: '40', value: '40' },
+  { key: 50, text: '50', value: '50' },
+  { key: 60, text: '60', value: '60' },
+  { key: 70, text: '70', value: '70' },
+  { key: 80, text: '80', value: '80' },
+  { key: 90, text: '90', value: '90' },
+  { key: 100, text: '100', value: '100' }
+];
